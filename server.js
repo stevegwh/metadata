670d8fac0d75fa9976d8001bb36775e145a35d12
Steve <styvwheeler@gmail.com>
2017-03-14 00:59:32 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser= require('body-parser');
+var multer  = require('multer')
+var upload = multer({ dest: 'uploads/' })
+var path = require('path');
+
+const app = express();
+
+app.set('view engine', 'ejs')
+
+app.listen(3000, () => {
+  console.log('Now listening to port 3000')
+})
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.get("/", (req, res)=> {
+  res.render('index.ejs', {filesize: null});
+})
+
+app.post('/file', upload.single('userFile'), function(req,res){
+  res.json({size: JSON.stringify(req.file.size)});
+
+	res.status(204).end();
+
+});
